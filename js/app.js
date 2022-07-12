ac9d1e5805b77224ae50ccaec0f2883dd6e1e27b
Abisoye Akinloye <abisoyesam@gmail.com>
2022-07-12 20:11:36 +0100

    evaluate the neural network

--- js/app.js
+++ js/app.js
@@ -8,19 +8,32 @@ const networkCtx = networkCanvas.getContext('2d');
 
 const road = new Road(carCanvas.width/2, carCanvas.width*0.95);
 
-const N = 100;
+const N = 1;
 const cars = generateCars(N);
 let bestCar = cars[0];
 
 if(localStorage.getItem("bestBrain")){
-    bestCar.brain = JSON.parse(
-        localStorage.getItem("bestBrain")
-    );
+    for (let i = 0; i < cars.length; i++) { 
+        cars[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        );
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain, 0.1);
+        }
+    }
 }
 
 const traffic = [
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY", 2)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY", 2)
 ];
+
+// TODO: Math.floor(Math.random()*3) to populate traffic 
 
 // car.draw(ctx);
 
--- js/network.js
+++ js/network.js
@@ -19,6 +19,25 @@ class NeuralNetwork{
         }
         return outputs
     }
+
+    static mutate(network, amount=1){
+        network.levels.forEach(level => {
+            for (let i=0; i < level.biases.length; i++){
+                level.biases[i] = lerp(
+                    level.biases[i],
+                    Math.random()*2 -1,
+                    amount
+                )
+            }
+            for (let i=0; i<level.weights; i++){
+                for (let j = 0; j < level.weights[i].length; j++){
+                    level.weights[i][j],
+                    Math.random()*2 -1,
+                    amount
+                }
+            }
+        });
+    }
 }
 
 class Level{
